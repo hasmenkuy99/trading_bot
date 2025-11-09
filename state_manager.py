9920138341a18dfee2b3eb43f80e2c16fa4426f4
jhonwick547 <58405054+jhonwick547@users.noreply.github.com>
2025-11-09 23:52:43 +0530

    Create state_manager.py

---
+++ state_manager.py
@@ -0,0 +1,21 @@
+# state_manager.py
+import json
+
+class StateManager:
+    def __init__(self, data):
+        self.data = data
+    @classmethod
+    def load(cls, path):
+        try:
+            with open(path, 'r') as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            data = {'balance': None, 'positions': []}
+        return cls(data)
+    def update(self, balance, positions):
+        self.data['balance'] = balance
+        self.data['positions'] = positions
+    @staticmethod
+    def save(path, data):
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=2)
